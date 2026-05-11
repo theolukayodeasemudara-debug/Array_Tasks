96c3affca78911a4ee8e433b71c229d3f4285a19
Olukayode <theolukayodeasemudara@gmail.com>
2026-05-11 13:34:45 +0100

    task two reversed array number

---
+++ ReverseArray.java
@@ -0,0 +1,26 @@
+// collect 10 integers from a user
+// use a loop to collect 10 integers and store them in the array we already initialised
+// reverse the array without creating a second array
+// display the reversed array
+
+public class ReverseArray{
+    public static void main(String[] args){
+        java.util.Scanner inputCollector = new java.util.Scanner(System.in);
+        
+        int[] arrayList = new int[10];
+        
+        for(int count = 0; count < arrayList.length; count++){
+            System.out.print("enter a number: ");
+            int number = inputCollector.nextInt();
+            arrayList[count] = number;
+            // number = arrayList[count].length() - 1;
+        }
+        
+        for(int index = 0; index < arrayList.length; index++){
+            System.out.print(arrayList[index] + " ");
+        }
+        
+        
+        
+    }
+}
